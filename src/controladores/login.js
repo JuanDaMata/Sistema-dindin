84eb335071f91bcf3d125ae56e620b4ae1fa025c
jose igor santos oliveira <jose.isoliveira@souunit.com.br>
2023-10-05 12:57:09 -0300

    Login finalizado

--- src/controladores/login.js
+++ src/controladores/login.js
@@ -10,9 +10,25 @@ const fazerLogin = async (req, res, next) => {
         return res.status(400).json({ mensagem: "Os campos email e senha são obrigatórios!" });
     }
     try {
+        const usuarioExiste = await pool.query(
+            'SELECT * FROM usuarios WHERE email = $1', [email]
+        );
+        if (usuarioExiste.rowCount === 0) {
+            return res.status(400).json({ mensagem: "Email e/ou senha inválido(s)!" })
+        }
+        if (!await bcrypt.compare(senha, usuarioExiste.rows[0].senha)) {
+            return res.status(400).json({ mensagem: "Email e/ou senha inválido(s)." });
+        }
 
+        const { senha: senhaCriptografada, ...usuario } = usuarioExiste.rows[0];
+        const token = jwt.sign(usuario, chaveSecreta, { expiresIn: "1d" });
+
+        return res.status(200).json({
+            usuario,
+            token
+        });
     } catch (error) {
-
+        return res.status(400).json({ mensagem: "Usuário e/ou senha inválido(s)." });
     }
 };
 module.exports = fazerLogin;
